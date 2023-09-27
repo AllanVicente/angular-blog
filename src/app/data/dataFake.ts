0905bba84e962a0b789625acf22ac76c1ddda8d9
Allan Miranda Note <miranda.allan@hotmail.com>
2023-09-27 13:07:19 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+  "id":"1",
+  "title":"NOTICIA EXEMPLO",
+  "description":"Marvel anuncia um novo filme do Homem de Ferro, confira",
+  "photoCover":"https://wallpapers.com/images/hd/iron-man-pictures-k83lqhe6vnh95b1m.jpg"
+
+},
+{
+  "id":"2",
+  "title":"Novo filme de Pantera Negra",
+  "description":"Chega aos cinemas novo filme do Pantera Negra após a morte do ator Chadwick Boseman ",
+  "photoCover":"https://itajaishopping.com.br/wp-content/uploads/2023/02/capa-pantera-blog.jpg"
+
+},
+{
+  "id":"3",
+  "title":"2ª Temporada de Loki ",
+  "description":"Chega ao Disney+ anova temporada de Loki ",
+  "photoCover":"https://cinepop.com.br/wp-content/uploads/2022/09/FcUPBihaMAAhlM1.jpg"
+},
+{
+  "id":"4",
+  "title":"2ª Temporada de Loki ",
+  "description":"Chega aos cinemas o terceiro filme da franquia",
+  "photoCover":"https://s2-techtudo.glbimg.com/E-v8xnunDY0WI5ZbVKR3U5FrxO8=/0x0:1068x655/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/x/A/W4CpeFTUuqJy2ERdGmPQ/guardioes-da-galaxia-3-1068x655.jpg"
+},
+]
